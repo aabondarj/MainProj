f8650d5a75d89201f4a36fdd194e73297b5fb22e
Aleksei <wx200223@gmail.com>
2023-01-20 01:45:42 +0300

    getElements

--- third/HW.js
+++ third/HW.js
@@ -4,6 +4,30 @@
 // confirm("Anime");
 // prompt("Insta", "samka");
 
+let startBtn = document.querySelector('#start'),
+    budgetValue = document.querySelector('.budget-value'),
+    dayBudgetValue = document.querySelector('.daybudget-value'),
+    levelValue = document.querySelector('.level-value'),
+    expensesValue = document.querySelector('.expenses-value'),
+    optionalExpensesValue = document.querySelector('.optionalexpenses-value'),
+    incomeValue = document.querySelector('.income-value'),
+    monthSavingsValue = document.querySelector('.monthsavings-value'),
+    yearSavingsValue = document.querySelector('.yearsavings-value'),
+
+    expensesItem = document.querySelectorAll('.expenses-item'),
+    btn = document.getElementsByTagName('button'),
+    expensesItemBtn = btn[0],
+    optionalExpensesBtn = btn[1],
+    countBudgetBtn = btn[2],
+    optionalExpensesItem = document.querySelectorAll('.optionalexpenses-item'),
+    chooseIncome = document.querySelector('.choose-income'),
+    savings = document.querySelector('#savings'),
+    chooseSum = document.querySelector('.choose-sum'),
+    choosePercent = document.querySelector('.choose-percent'),
+    yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value'),
+    dayValue = document.querySelector('.day-value');
+
 let money, time;
 
 function start() {
